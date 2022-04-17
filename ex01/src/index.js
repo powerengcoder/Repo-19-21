278339cc51e4684e7f0d716de8a99075aee5139c
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-17 18:22:59 +0200

    Add ex 01

---
+++ ex01/src/index.js
@@ -0,0 +1,31 @@
+var print = "";
+var t = "";
+var back = false;
+var caps = false;
+
+function button_clicked_caps() {
+  if (caps) caps = false;
+  else {
+    caps = true;
+  }
+}
+
+function button_clicked(character) {
+  if (caps) {
+    print = print + character;
+  } else {
+    print = print + character.toLowerCase();
+  }
+  var output = document.getElementById("input");
+  output.value = print;
+}
+
+function button_clicked_back() {
+  var output = document.getElementById("input");
+  output.value = output.value.substring(0, output.value.length - 1);
+  print = "";
+}
+
+function button_clicked_ok() {
+alert(document.getElementById('input').value);
+}
